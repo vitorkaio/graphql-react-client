6d4ba3a03c1945052b91ec40d17ae0f07ecbe884
vitorkaio <vitorcaiodepaula@gmail.com>
2019-08-20 12:56:19 -0300

    react with subs

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,14 +1,43 @@
-import ApolloClient from 'apollo-boost';
+import { ApolloClient } from 'apollo-client';
 import gql from 'graphql-tag';
 
-const getClient = async () => {
+import { getMainDefinition } from 'apollo-utilities';
+import { split } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+
+export const getClient = async () => {
+  const httpLink = new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+    
+  const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4000',
+    options: {
+      reconnect: true
+    }
+  })
+    
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    httpLink
+  )
+    
   return new ApolloClient({
-    uri: 'http://localhost:3003/',
-  });
+    link,
+    cache: new InMemoryCache(),
+    connectToDevTools: true,
+  })
 }
 
 const GET_USER = gql`
-  {
+  query User {
     user {
       name age
     }
@@ -17,6 +46,7 @@ const GET_USER = gql`
 
 export const getUser = async () => {
   const client = await getClient()
+  userSubs(client)
   const res = await client.query({
     query: GET_USER,
   })
@@ -30,10 +60,8 @@ export const getUser = async () => {
 const CREATE_USER = gql`
   mutation CreateUser($name: String!, $age: Int!) {
     createUser(
-      data: {
         name: $name
         age: $age
-      }
     ) {
       name age
     }
@@ -51,6 +79,26 @@ export const createUser = async (user) => {
   })
   if (res) return res.data.createUser
   return null
+}
+
+
+const USER_SUBS = gql`
+  subscription UserAdded {
+    userAdded {
+      name age
+    }
+  }
+`;
+
+export const userSubs = (client) => {
+  client.subscribe({
+    query: USER_SUBS,
+  }).subscribe({
+    next(data) {
+      console.log(data)
+    },
+    error(err) { console.error('err', err); },
+  });
 }
 
 
